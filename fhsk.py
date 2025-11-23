ee50964b606f122de413cfe4e278d208b8e4063b
hodieduvn-glitch <hodi.edu.vn@gmail.com>
2025-11-23 10:41:14 +0700

    Create fhsk.py

---
+++ fhsk.py
@@ -0,0 +1,7 @@
+ msg = f"⚠️ Không tìm thấy chuỗi trong: {full_path}"
+                        print(msg)
+                        log_entries.append(msg)
+                except Exception as e:
+                    msg = f"❌ Lỗi với file {full_path}: {e}"
+                    print(msg)
+                    log_entries.append(msg)
